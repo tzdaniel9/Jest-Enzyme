85681d10b84062dabd51e641228f3c6950e81abe
tzdaniel9 <36230940+tzdaniel9@users.noreply.github.com>
2020-08-31 21:42:20 +0200

    Started testing

---
+++ click-counter/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ click-counter/src/App.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+import App from './App';
+import { waitForElementToBeRemoved } from '@testing-library/react';
+
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+test('renders without error',()=>{
+  const wrapper = shallow(<App/>);
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button',()=>{
+
+});
+
+test('renders counter display',()=>{
+
+});
+
+test('counter starts at 0',()=>{
+
+})
+
+test('clickingvutton increments counter display',()=>{
+
+});
